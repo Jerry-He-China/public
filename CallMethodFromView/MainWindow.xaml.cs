d96612c78ab6aa59bfbbec2cb952421618cdad89
Jerry.He <Jerry.He.China@hotmail.com>
2018-10-30 16:54:30 +0800

    CallMethod Demo

---
+++ CallMethodFromView/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace CallMethodFromView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new MainWindowViewModel();
+        }
+    }
+
+    public class MainWindowViewModel
+    {
+        public void Submit()
+        {
+            MessageBox.Show("Submit");
+        }
+    }
+
+}
